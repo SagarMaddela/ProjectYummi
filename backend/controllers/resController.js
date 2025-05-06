8b18c482d8d149df263bd2d74656d9226400384a
bhargav-1729 <bhargavreddy.k22@iiits.in>
2025-05-07 01:11:27 +0530

    image buffer1

--- backend/controllers/resController.js
+++ backend/controllers/resController.js
@@ -68,7 +68,7 @@ exports.createRestaurant = async (req, res) => {
       // Hash password
       const salt = await bcrypt.genSalt(10);
       const hashedPassword = await bcrypt.hash(password, salt);
-
+      console.log("hello");
       // Create restaurant
       const restaurant = new Restaurant({
           restaurantName,
